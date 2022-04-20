79a97844096c0066eace83d6e60dd38937dc6ac2
Lanie Williams <lanie_williams@baylor.edu>
2022-04-20 13:53:23 -0500

    setting up django

---
+++ manage.py
@@ -0,0 +1,25 @@
+#this is the file that runs the whole program
+#if your database does exist and you make a change to your model, you need to migrate those changes.
+# migrate either creates a database or migrates changes
+
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks.""" 
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
