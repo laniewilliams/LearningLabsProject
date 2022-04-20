5bdd79a69d4a8640f0afd3debee9cdec47af5ea8
Lanie Williams <lanie_williams@baylor.edu>
2022-04-20 14:15:50 -0500

    admin, topics, entries

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Topic, Entry #have to import it before you register it
+
+admin.site.register(Topic)
+admin.site.register(Entry)
--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model): #inheriting all the properties of the models class
+    text = models.CharField(max_length=200) #name of the topic
+    date_added = models.DateTimeField(auto_now_add=True) #we don't have to explicitly say the date, it will automatically display the current date
+
+    def __str__(self): #allows us to return actual information
+        return self.text #if you said print(Topic) it would now give you the name of the topic
+
+
+class Entry(models.Model): #have to associate entries with topic
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE) #many entries to one topic, entry is a foreign key to topic
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+    return f'{self.text[:50]}...' #show a ... after 50 characters 
+
