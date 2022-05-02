da0538fa80a0341916a4a3b1188e42e8ba936e97
Lanie Williams <lanie_williams@baylor.edu>
2022-05-02 13:38:33 -0500

    adding new entry

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic, Entry
 
 
 class TopicForm(forms.ModelForm):
@@ -9,3 +9,11 @@ class TopicForm(forms.ModelForm):
         model = Topic
         fields = ['text'] #only need the text field
         labels = {'text':''} #can change this to whatever you want
+
+class EntryForm(forms.ModelForm):
+    #if you want to create custom fields you would do it here
+    class Meta: #using the meta class bc we already have a model defined called forms. we don't have to redefine what type of field we want
+        model = Entry
+        fields = ['text'] #only need the text field
+        labels = {'text':''} #can change this to whatever you want
+        widget = {'text':forms.Textarea(attrs={'cols':80})} #making the text field a text area. This is going to make it look like a big box
