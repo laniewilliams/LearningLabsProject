f157b8d2388eab385dcf7f2e74f0990e041975e2
Lanie Williams <lanie_williams@baylor.edu>
2022-05-04 07:43:02 -0500

    register

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 
 
-
+from . import views
 app_name = 'users'
 
 
 urlpatterns = [
     path('',include('django.contrib.auth.urls')), #don't have to create a view because django has a default view. we do have to create a template
-
+    # registration page
+    path('register/',views.register,name='register'),
 ]
